df1f718a1ac0f63b68528b78bece41ad61482451
Julio Gudino <julio.gudino@contra.com>
2024-01-15 17:02:15 +0000

    feat: section 1 no anim

--- src/app/components/PasoUno/PasoUno.tsx
+++ src/app/components/PasoUno/PasoUno.tsx
@@ -1,6 +1,9 @@
 'use client';
+import Silueta from '../../../../public/images/silueta.png';
 import { Section } from '../Section/Section';
-import { useInView } from 'framer-motion';
+import { Shapes } from './Shapes';
+import { motion, useInView } from 'framer-motion';
+import Image from 'next/image';
 import { useRef } from 'react';
 
 const animationDuration = 1.5;
@@ -11,8 +14,6 @@ export const PasoUno = () => {
   const isInView = useInView(ref, {
     amount: 0.9,
   });
-
-  console.log('isInView', isInView);
 
   return (
     <Section
@@ -31,13 +32,33 @@ export const PasoUno = () => {
       }}
     >
       <div className="flex justify-center align-center h-full p-[12%]">
-        <div className="flex flex-row gap-12">
-          <h2 className="text-8xl text-[rgb(var(--section-two-text-color-rgb))] font-bold">
-            1
-          </h2>
-          <h2 className="text-6xl text-[rgb(var(--section-two-text-color-rgb))] ">
-            SIÉNTETE LIBRE <br /> Y SEGUR@ DE TI MISM@
-          </h2>
+        <div className="flex flex-col gap-4">
+          <div className="flex flex-row gap-12 w-100 justify-center">
+            <h2 className="text-8xl text-[rgb(var(--section-two-text-color-rgb))] font-bold">
+              1
+            </h2>
+            <motion.h2 className="text-6xl text-[rgb(var(--section-two-text-color-rgb))] ">
+              SIÉNTETE LIBRE <br /> Y SEGUR@ DE TI MISM@
+            </motion.h2>
+          </div>
+          <div className="flex flex-row gap-32 items-center">
+            <Image
+              alt="Silueta"
+              height={420}
+              src={Silueta}
+              width={420}
+            />
+            <p className="text-2xl max-w-sm font-varela">
+              Si estás{' '}
+              <span className="text-section-two-text">hasta la madre </span>
+              de estar mal, es momento de sentirte y verte bien.
+              <br />
+              <br />
+              Aprovecha cada momento para ser feliz contigo y{' '}
+              <span className="text-section-two-text">no me jodas</span> con que
+              no se puede. INTÉNTALO.
+            </p>
+          </div>
         </div>
       </div>
     </Section>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,27 @@
-import type { Config } from 'tailwindcss'
+/* eslint-disable canonical/filename-match-exported */
+import { type Config } from 'tailwindcss';
 
-const config: Config = {
+const tailwindConfig: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  plugins: [],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+      },
+      colors: {
+        'section-two-text': 'rgb(var(--section-two-text-color-rgb))',
+      },
+      fontFamily: {
+        varela: ['varela', 'sans-serif'],
       },
     },
   },
-  plugins: [],
-}
-export default config
+};
+export default tailwindConfig;
